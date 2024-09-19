25fd64d981750aac5ab8aab4cadbbf9f182153d3
Jeremy Reeder <4392275+jeremyreeder@users.noreply.github.com>
2024-09-19 11:33:59 -0600

    Update Attendee.js

--- code/src/AttendeeSite/Attendee.js
+++ code/src/AttendeeSite/Attendee.js
@@ -1,3 +1,18 @@
 function attendeeName() {
   document.getElementById("Name").innerHTML = window.location.search;
+  
+  const express = require('express')
+  const router = express.Router()
+  
+  router.get('/greeting', (req, res) => {
+      const { name }  = req.query;
+      res.send('<h1> Hello :'+ name +"</h1>")
+  })
+  
+  router.get('/greet-template', (req,res) => {
+      name = req.query.name
+      res.render('index', { user_name: name});
+  })
+  
+  module.exports = router
 }
